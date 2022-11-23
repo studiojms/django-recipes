8af21c80c3f13290d85a0db96ff94655e66eae2e
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2022-11-23 12:57:02 -0300

    feat: added create user

---
+++ app/user/views.py
@@ -0,0 +1,11 @@
+"""
+View for the user API
+"""
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Creates a new user in the system"""
+    serializer_class = UserSerializer
