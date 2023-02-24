9336b6798b1bdafebae8c3c9bbc1bb0586d516e6
Jefferson Mariano de Souza <158855+studiojms@users.noreply.github.com>
2023-02-24 19:45:24 +0000

    Added listing recipe api

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ To run some command, execute it from the container like
 ```sh
 docker compose run --rm app sh -c "command"
 ```
+
+### Running tests inside dev-container
+
+```sh
+cd app
+python manage.py test
+```
+
+### Creating migrations
+
+```sh
+python manage.py makemigrations
+```
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,11 +1,13 @@
 """
 Tests for models
 """
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 
-class ModelTEsts(TestCase):
+class ModelTests(TestCase):
     """Test models"""
 
     def test_create_user_with_email_successful(self):
@@ -45,3 +47,11 @@ class ModelTEsts(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user('test@example.com', 'testpass123')
+        recipe = models.Recipe.objects.create(
+            user=user, title='Sample recipe name', time_minutes=5, price=Decimal('5.50'), description='Sample description')
+
+        self.assertEqual(str(recipe), recipe.title)
